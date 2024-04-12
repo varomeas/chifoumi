c11a907128511bcd2d33efd508dc3ab632b2f356
JulienF777 <juju56260@live.fr>
2024-04-12 18:24:22 -0400

    sons ajoutés

--- Assets/Scripts/Joueur.cs
+++ Assets/Scripts/Joueur.cs
@@ -18,6 +18,12 @@ public class Joueur : MonoBehaviour
     public int score;
     public int scoremultiplier;
 
+    //sounds
+    public AudioClip[] destroySounds; // Array to hold your sound effects
+    public AudioSource audioSource; // Reference to the AudioSource component
+
+    public AudioClip hitSound; // Array to hold your sound effects
+
     private void Update()
     {
         // Utilisez le système d'entrée Unity pour détecter les touches F, G et H
@@ -42,7 +48,15 @@ public class Joueur : MonoBehaviour
         foreach (GameObject target in targets)
         {
             if (target.name.StartsWith(targetName) && target.name.EndsWith("(Clone)"))
-            {
+            {   
+                //play a random sound from the destroySounds array
+                int soundIndex = Random.Range(0, destroySounds.Length);
+                if (audioSource && destroySounds.Length > 0)
+                {
+                    audioSource.PlayOneShot(destroySounds[soundIndex]);
+                }
+                
+                //destroy the projectile
                 Destroy(target);
                 score += 100*scoremultiplier;
                 scoremultiplier += 1;
@@ -74,6 +88,16 @@ public class Joueur : MonoBehaviour
         scoremultiplier = 1;
         Debug.Log("currentHealth"+currentHealth);
         Debug.Log("maxHealth"+maxHealth);
+
+        // Play the music using the existing AudioSource
+        if (audioSource != null && audioSource.clip != null)
+        {
+            audioSource.Play();
+        }
+        else
+        {
+            Debug.LogError("No AudioClip assigned to the existing AudioSource!");
+        }
     }
 
     public void TakeDamage(int damage)
@@ -82,6 +106,13 @@ public class Joueur : MonoBehaviour
         Debug.Log("Player hit"+currentHealth);
         //reset the score multiplier
         scoremultiplier = 1;
+
+        //play the hit sound effect
+        if (audioSource && hitSound)
+        {
+            audioSource.PlayOneShot(hitSound);
+        }
+        
         if (currentHealth <= 0)
         {
             Die();
@@ -102,6 +133,9 @@ public class Joueur : MonoBehaviour
         GameScoreScreen.SetActive(false);
         RestartButton.SetActive(true);
         // Vous pouvez ajouter d'autres actions ici, comme la réinitialisation du niveau, l'affichage d'un message de défaite, etc.
+
+        //stop audioclip music
+        audioSource.Stop();
     }
 
     //when the player collides with an object
